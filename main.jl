cc55d3576cbbafcf4925c97362e5337392c4c154
Roger-luo <hiroger@qq.com>
2018-06-06 17:27:13 +0800

    add optim

--- main.jl
+++ main.jl
@@ -1,1 +1,23 @@
 include("load.jl")
+
+using Yao, UnicodePlots, BenchmarkTools, GradOptim
+
+function train!(qcbm::QCBM, ptrain, optim; learning_rate=0.1, maxiter=100)
+    initialize!(qcbm)
+    kernel = Kernels.RBFKernel(nqubits(qcbm), [0.25], false)
+    history = Float64[]
+
+    for i = 1:maxiter
+        grad = gradient(qcbm, kernel, ptrain)
+        curr_loss = loss(qcbm, kernel, ptrain)
+        push!(history, curr_loss)
+        println(i, " step, loss = ", curr_loss)
+
+        # Warn: we need a primitive block to enable
+        # BLAS here.
+        params = parameters(qcbm)
+        update!(params, grad, optim)
+        dispatch!(qcbm, params)
+    end
+    history
+end
---
+++ modules/Utils.jl
@@ -0,0 +1,21 @@
+module Utils
+
+function gaussian_pdf(n, μ, σ)
+    x = collect(1:1<<n)
+    pl = @. 1 / sqrt(2pi * σ^2) * exp(-(x - μ)^2 / (2 * σ^2))
+    pl / sum(pl)
+end
+
+function get_nn_pairs(n)
+    pairs = []
+    for inth in 1:2
+        for i in inth:2:n
+            push!(pairs, i=>i % n + 1)
+        end
+    end
+    pairs
+end
+
+
+
+end
